25e359eceac6c5aa8b4255e377e10f1c786e4f22
Aswin-04 <e.aswin004@gmail.com>
2026-02-24 10:32:19 +0530

    refactor(layout): move Header component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Urbanist } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/header";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${urbanist.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
