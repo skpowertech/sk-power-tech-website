e73c403dde6e3ad046d556e5225c995dbd65f989
Aswin-04 <e.aswin004@gmail.com>
2026-02-25 14:33:52 +0530

    feat(ui): add SectionHeader component

---
+++ components/ui/section-header.tsx
@@ -0,0 +1,20 @@
+interface SectionHeaderProps {
+  label: string,
+  title: string,
+  description: string
+}
+
+
+const SectionHeader = ({label, title, description}: SectionHeaderProps) => {
+  return (
+    <div className="flex flex-col gap-4 items-start md:items-center md:text-center" >
+      <span className='w-fit uppercase border border-zodiac-400 text-sm font-semibold bg-zodiac-50 text-zodiac-700 px-5 py-2 rounded-full' >
+        {label}
+      </span>
+      <h2 >{title}</h2>
+      <p className="text-zinc-600 max-w-2xl" >{description}</p>
+    </div>
+  )
+}
+
+export default SectionHeader
