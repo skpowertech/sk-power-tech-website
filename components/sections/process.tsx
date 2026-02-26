2907cd97b9a790a9f95c098f5e1ff3b18f2ff564
Aswin-04 <e.aswin004@gmail.com>
2026-02-26 10:45:24 +0530

    feat(processes): add Processes section

--- components/sections/process.tsx
+++ components/sections/process.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import SectionHeader from '../ui/section-header'
+import TimelineCard from '../ui/timeline-card'
+import { processes } from '@/lib/constants'
 
 const Process = () => {
   return (
-    <div>Process</div>
-  )
+    <section className='max-container padding space-y-12'>
+      <SectionHeader
+        label='Process'
+        title='How We Work'
+        description='Three simple steps from consultation to complete protection'
+      ></SectionHeader>
+      <ul className='w-fit mx-0 sm:mx-auto'>
+        {processes.map((process) => (
+          <li key={process.step}>
+            <TimelineCard {...process}></TimelineCard>
+          </li>
+        ))}
+      </ul>
+    </section>
+  ) 
 }
 
 export default Process
