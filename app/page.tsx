16b29a3c1b8bfe2b20399f2efd00199187e10ff3
Aswin-04 <e.aswin004@gmail.com>
2026-02-26 10:47:18 +0530

    refactor(home): add AdaptableSolutions Process OurWork sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import WhatYouGet from "@/components/sections/what-you-get";
 import Hero from "@/components/sections/hero";
 import WhyChooseUs from "@/components/sections/why-choose-us";
 import OurServices from "@/components/sections/our-services";
+import OurWork from "@/components/sections/our-work";
+import AdaptableSolutions from "@/components/sections/adaptable-solutions";
+import Process from "@/components/sections/process";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <WhyChooseUs/>
       <WhatYouGet/>
       <OurServices/>
+      <AdaptableSolutions/>
+      <Process/>
+      <OurWork/>
     </div>
   );
 }
