3d9fa44db30b21196996b554eaff4a9994d43b54
Aswin-04 <e.aswin004@gmail.com>
2026-02-27 10:53:00 +0530

    feat(home): add CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import AdaptableSolutions from "@/components/sections/adaptable-solutions";
 import Process from "@/components/sections/process";
 import AboutSection from "@/components/sections/about-section";
 import { about } from "@/lib/constants";
+import CTASection from "@/components/sections/cta-section";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Process/>
       <OurWork/>
       <AboutSection {...about}/>
+      <CTASection/>
     </div>
   );
 }
--- components/sections/cta-section.tsx
+++ components/sections/cta-section.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import CTAButton from "../ui/cta-button"
 
 const CTASection = () => {
   return (
-    <div>CTASection</div>
+    <section className="max-container padding flex flex-col gap-8 items-center text-center">
+      <h2 className="" >Ready to Power and Protect <span className="text-zodiac-900 block" >Your Business?</span></h2>  
+      <p className="text-zinc-500">Speak directly with our engineers today — call or WhatsApp for expert guidance.</p>
+      <div className="flex flex-col gap-4 justify-center sm:flex-row sm:gap-8 ">
+        <CTAButton variant={'outline_dark'} size={'lg'} ></CTAButton>
+        <CTAButton variant={'filled'} size={'lg'} ></CTAButton>
+      </div>
+    </section>
   )
 }
 
